8ce25715b83ad25f465098e27acc5f78262d9cdb
aashi1408 <aashiv2525@gmail.com>
2025-12-13 23:03:26 +0530

    Implement twoSum function in two_sum.cpp
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ two_sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        vector<int> v;
+        for(int j=0 ; j<=n-2 ; j++)
+        {
+            for(int i=j+1 ; i<=n-1 ; i++)
+            {
+                if(nums[j] + nums[i] == target)
+                {
+                    v.push_back(j);
+                    v.push_back(i);
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
