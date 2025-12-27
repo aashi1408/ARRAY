b70c6e852b8168469008bdfcfd57b1e0eaf8c89b
aashi1408 <aashiv2525@gmail.com>
2025-12-27 17:28:48 +0530

    Create intersection_of_two_arrays.cpp
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [9,4]
    Explanation: [4,9] is also accepted.
     
    
    Constraints:
    
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 1000

---
+++ intersection_of_two_arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int freq[1001] = {0};//to track if an element has occures or not
+        //to maintain the uniqueness of the ans array
+        vector<int> ans;
+
+        for(int i=0 ; i<n ; i++)
+        {
+            for(int j=0 ; j<m ; j++)
+            {
+                if(nums1[i]==nums2[j] && freq[nums1[i]]==0)
+                {
+                    freq[nums1[i]] = 1;
+                    ans.push_back(nums1[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
