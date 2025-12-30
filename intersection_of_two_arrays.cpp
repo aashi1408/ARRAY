69e2725e7336ed2781a8191e9f426c8c5ef30a07
aashi1408 <aashiv2525@gmail.com>
2025-12-30 21:25:55 +0530

    Update intersection_of_two_arrays.cpp

--- intersection_of_two_arrays.cpp
+++ intersection_of_two_arrays.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
-        int freq[1001] = {0};//to track if an element has occures or not
+        int freq[1001] = {0};//to track if an element has occurs or not
         //to maintain the uniqueness of the ans array
         vector<int> ans;
 
@@ -16,8 +16,8 @@ public:
                 if(nums1[i]==nums2[j] && freq[nums1[i]]==0)
                 {
                     freq[nums1[i]] = 1;
-                    ans.push_back(nums1[i]);
-                    break;
+                    ans.push_back(nums1[i]);//turant push back karo ele ko
+                    break;//no need to further iterate
                 }
             }
         }
