79cb33cf429697b5b97bc93970869bc2eeb1d9d6
aashi1408 <aashiv2525@gmail.com>
2026-01-13 16:24:54 +0530

    Fix formatting in intersection function declaration
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2]
    Example 2:
    
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [9,4]
    Explanation: [4,9] is also accepted.
     
    
    Constraints:
    
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 1000                                
    
    Do you have a better approach then O(n^2)
    find more optimized solution.

--- intersection_of_two_arrays.cpp
+++ intersection_of_two_arrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)   
     {
         int n = nums1.size();
         int m = nums2.size();
